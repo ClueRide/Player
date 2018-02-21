fd1671fc74e59ec9ad2b106a3d7bb9c209a946a7
jett <jettmarks@gmail.com>
2018-02-20 21:02:10 -0500

    PLAY-3 Prepare More Pages for the Game
    * Rolling
    * Location
    * Puzzle (which may also serve as "answer summary")
    
    As well as adding Rolling to the menu.

---
+++ src/pages/rolling/rolling.module.ts
@@ -0,0 +1,13 @@
+import { NgModule } from '@angular/core';
+import { IonicPageModule } from 'ionic-angular';
+import { RollingPage } from './rolling';
+
+@NgModule({
+  declarations: [
+    RollingPage,
+  ],
+  imports: [
+    IonicPageModule.forChild(RollingPage),
+  ],
+})
+export class RollingPageModule {}
