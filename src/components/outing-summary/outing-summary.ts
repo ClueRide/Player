cde929a408bd7877e74268774f837155a761e797
jett <jettmarks@gmail.com>
2019-03-18 21:00:12 -0400

    Home Page showing wrong Outing Details
    - Aligns the Outing Summary with the new structure of the OutingView.

--- src/components/outing-summary/outing-summary.ts
+++ src/components/outing-summary/outing-summary.ts
@@ -1,35 +1,46 @@
-import { Component } from '@angular/core';
+import {AfterViewInit, Component} from '@angular/core';
 import {NavController} from "ionic-angular";
+import {OutingService, OutingView} from "front-end-common";
+import {Subscription} from "rxjs/Subscription";
 
 /**
- * Generated class for the OutingSummaryComponent component.
+ * Displays selected details from the Outing sufficient for the player to know
+ * where to be and when the game starts.
  *
- * See https://angular.io/api/core/Component for more info on Angular
- * Components.
+ * Also provides links to more information about the course and a button to bring
+ * up the full Outing Page.
  */
 @Component({
   selector: 'outing-summary',
   templateUrl: 'outing-summary.html'
 })
-export class OutingSummaryComponent {
+export class OutingSummaryComponent
+implements AfterViewInit {
 
-  outing = {
-    id: 12,
-    course: {
-      url: "five-free-things",
-      name: "Five Free Things"
-    },
-    date: "Saturday, March 3, 2018",
-    time: "10AM - 1PM"
-  };
+  outing: OutingView;
+  private outingSubscription: Subscription;
 
   constructor(
-    public navCtrl: NavController
+    public navCtrl: NavController,
+    private outingService: OutingService,
   ) {
+    this.outing = <any>{};
+  }
+
+  ngAfterViewInit() {
+    this.outingSubscription = this.outingService.getSessionOuting().subscribe(
+      (response) => {
+        this.outing = response;
+      }
+    );
   }
 
   public viewDetails() {
     this.navCtrl.push("OutingPage");
+  }
+
+  ngOnDestroy() {
+    this.outingSubscription.unsubscribe();
   }
 
 }
